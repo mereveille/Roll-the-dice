0534bb2bdf548a3a98fecef1abb4f8fba11b6f72
Your Name <latreche.sonia95@gmail.com>
2023-05-15 12:47:11 +0200

    reorganisation of the image let to display the score under the dice

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 
 /* Je récupère toutes mes données html et déclare mes variables*/
-let images =["IMG/one-g37ddbed93_640.png", 
-             "IMG/dice-g2595bb808_640.png", 
-             "IMG/three-gb0a4fb5a3_640.png",
-             "IMG/dice-gf2f8bf668_640.png",
-             "IMG/dice-ga29ceb950_640.png",
-            "IMG/dice-ga9027fdae_640.png"];
-let dice = document.querySelectorAll("img");
+
 let player1 = document.querySelector("player1");
 let player2 = document.querySelector("player2");
 let global_p1 = document.querySelector("global_p1");
@@ -15,15 +9,13 @@ let round_p1 = document.querySelector("round_p1");
 let round_p2 = document.querySelector("round_p2");
 let current = document.querySelector("#current");
 
-
-
-
-
-
-function reset(){
-    
-};
-
+let images =["IMG/one-g37ddbed93_640.png", 
+             "IMG/dice-g2595bb808_640.png", 
+             "IMG/three-gb0a4fb5a3_640.png",
+             "IMG/dice-gf2f8bf668_640.png",
+             "IMG/dice-ga29ceb950_640.png",
+            "IMG/dice-ga9027fdae_640.png"];
+let dice = document.querySelectorAll("img");
 function roll(){
     dice.forEach(function (die){
         die.classList.add("shake");
